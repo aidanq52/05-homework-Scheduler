8fbb125d8eef22e18a1fc8087ed4449941086473
Aidan Q <aidanq52@gmail.com>
2020-04-29 15:12:01 -0700

    more updates to make the code sharable

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -4,46 +4,80 @@ var taskArray=["this is the task for 9am","this is the task for 10am","this is t
                 "this is the task for 1pm", "this is the task for 2pm", "this is the task for 3pm", "this is the task for 4pm",
                 "this is the task for 5pm"];
 
-$('#inputForm').attr("value", taskArray[0]);
+// $('#inputForm').attr("value", taskArray[0]);
 
-$('#submitForm').on('click', function(event){
-    event.preventDefault();
+// $('#submitForm').on('click', function(event){
+//     event.preventDefault();
 
-    console.log(taskArray[0]);
+//     console.log(taskArray[0]);
 
-    var input = $('#inputForm').val().trim();
+//     var input = $('#inputForm').val().trim();
 
-    console.log(input);
+//     console.log(input);
 
-    taskArray[0] = input;
+//     taskArray[0] = input;
 
-    console.log(taskArray[0]);
+//     console.log(taskArray[0]);
 
-    localStorage.setItem('taskArray', JSON.stringify(taskArray));
-    JSON.parse(localStorage.getItem('taskArray'));
+//     localStorage.setItem('taskArray', JSON.stringify(taskArray));
+//     JSON.parse(localStorage.getItem('taskArray'));
 
-    console.log(taskArray[0])
+//     console.log(taskArray[0])
 
-});
+// });
 
-$('#inputForm2').attr("value", taskArray[1]);
+// $('#inputForm2').attr("value", taskArray[1]);
 
-$('#submitForm2').on('click', function(event){
-    event.preventDefault();
+// $('#submitForm2').on('click', function(event){
+//     event.preventDefault();
 
-    console.log(taskArray[1]);
+//     console.log(taskArray[1]);
 
-    var input = $('#inputForm2').val().trim();
+//     var input = $('#inputForm2').val().trim();
 
-    console.log(input);
+//     console.log(input);
 
-    taskArray[1] = input;
+//     taskArray[1] = input;
 
-    console.log(taskArray[1]);
+//     console.log(taskArray[1]);
 
-    localStorage.setItem('taskArray', JSON.stringify(taskArray));
-    JSON.parse(localStorage.getItem('taskArray'));
+//     localStorage.setItem('taskArray', JSON.stringify(taskArray));
+//     JSON.parse(localStorage.getItem('taskArray'));
 
-    console.log(taskArray[1])
+//     console.log(taskArray[1])
 
-});
+// });
+i = 3;
+
+$('.content-box').on('click', function(event){
+    event.preventDefault()
+
+    console.log(event.target.dataset.index);
+
+    i = event.target.dataset.index
+
+    console.log(taskArray[i]);
+
+    updater();
+})
+
+function updater(i){
+
+    console.log(taskArray[i]);
+
+    console.log($('.entry').data-index[i].val().trim())
+
+    // var input = $('.entry').data-index[i].val().trim();
+
+    // console.log(input);
+
+    // taskArray[0] = input;
+
+    // console.log(taskArray[0]);
+
+    // localStorage.setItem('taskArray', JSON.stringify(taskArray));
+    // JSON.parse(localStorage.getItem('taskArray'));
+
+    // console.log(taskArray[0])
+
+};
