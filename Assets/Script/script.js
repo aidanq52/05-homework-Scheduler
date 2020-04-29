77ac186e0a8ece1fd848a15aad59a7c6a8e30615
Aidan Q <aidanq52@gmail.com>
2020-04-29 13:32:22 -0700

    now I have a framework for adding text to the input boxes and just need to make it into a loop so I can condense the code

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,31 +1,49 @@
 $('#date').text(moment().format('dddd[,] MMMM Do')).css('text-align','center').css('margin','0px')
 
-var taskMatrix =   {entry1:    {hour: 9,
-                                task: 'this is the task for 9'},
-                    entry2:    {hour: 10,
-                                task: "this is the task for 10"}
-                
-}
+var taskArray=["this is the task for 9am","this is the task for 10am","this is the task for 11am","this is the task for 12pm",
+                "this is the task for 1pm", "this is the task for 2pm", "this is the task for 3pm", "this is the task for 4pm",
+                "this is the task for 5pm"];
 
-console.log(taskMatrix.entry1.task)
-
-savedTask=[];
+$('#inputForm').attr("value", taskArray[0]);
 
 $('#submitForm').on('click', function(event){
     event.preventDefault();
+
+    console.log(taskArray[0]);
 
     var input = $('#inputForm').val().trim();
 
     console.log(input);
 
-    taskMatrix.entry1.task.replace('this is the task for 9', input);
+    taskArray[0] = input;
 
-    console.log(taskMatrix.entry1.task);
+    console.log(taskArray[0]);
 
-    localStorage.setItem('savedTask', JSON.stringify(savedTask));
+    localStorage.setItem('taskArray', JSON.stringify(taskArray));
+    JSON.parse(localStorage.getItem('taskArray'));
 
-    $('#inputForm').text(JSON.parse(localStorage.getItem('savedTask')));
+    console.log(taskArray[0])
 
-    console.log(text(JSON.parse(localStorage.getItem('savedTask'))));
+});
+
+$('#inputForm2').attr("value", taskArray[1]);
+
+$('#submitForm2').on('click', function(event){
+    event.preventDefault();
+
+    console.log(taskArray[1]);
+
+    var input = $('#inputForm2').val().trim();
+
+    console.log(input);
+
+    taskArray[1] = input;
+
+    console.log(taskArray[1]);
+
+    localStorage.setItem('taskArray', JSON.stringify(taskArray));
+    JSON.parse(localStorage.getItem('taskArray'));
+
+    console.log(taskArray[1])
 
 });
