c3675ae2ba16d6252caa63616bd7d87556d9a20e
Aidan Q <aidanq52@gmail.com>
2020-04-30 13:32:03 -0700

    finished the save functionality time to more fully hook into the moment API and start adding some formatting based on that

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -6,56 +6,65 @@ var taskArray=["this is the task for 9am","this is the task for 10am","this is t
 
 init()
 
-$('.content-box').on('click', function(event){
+
+//adding content from the text boxes to local storage
+$('.save').on('click', function(event){
 
     event.preventDefault()
 
     var i = '';
 
-    console.log(event.target.dataset.index);
+    // console.log(event.target.dataset.index);
 
     i = event.target.dataset.index;
 
-    console.log(taskArray[i]);
+    // console.log(taskArray[i]);
 
     input = $('.entry').eq(i).val();
 
-    console.log(input);
+    // console.log(input);
 
     taskArray[i] = input;
 
-    console.log(taskArray[i]);
+    // console.log(taskArray[i]);
 
     localStorage.setItem("taskArray", JSON.stringify(taskArray));
 
-    var storedTaskArray = JSON.parse(localStorage.getItem("taskArray"));
+    // var storedTaskArray = JSON.parse(localStorage.getItem("taskArray"));
 
-    console.log(storedTaskArray);
+    // console.log(storedTaskArray);
 
 });
 
+
+//writing the initiation function to pull things from local storage
 function init(){
     var storedTaskArray = JSON.parse(localStorage.getItem("taskArray"));
-    console.log(storedTaskArray);
+    // console.log(storedTaskArray);
 
     if(storedTaskArray !== null){
         taskArray = storedTaskArray;
     }
 
-    console.log(taskArray);
+    // console.log(taskArray);
 
-    for(i=0;i<8;i++){
+    for(i=0;i<9;i++){
 
-        task = taskArray[i];
+        var task = taskArray[i];
 
-        console.log(task);
+        // console.log(task);
 
-        console.log($('.entry').eq[i].attr("value"))
-        $('.entry').eq[i].attr("value", task);
+        $('.entry').eq(i).val(task);
 
     }
 
 };
+
+//trying to figure out how to use moment API
+// .format('dddd[,] MMMM Do')
+// console.log(moment().format());
+// console.log(moment());
+// console.log()
 
 
 // $('#inputForm').attr("value", taskArray[0]);
@@ -105,7 +114,4 @@ function init(){
 // localStorage.removeItem("taskArray")
 // localStorage.removeItem('taskMatrix')
 
-// .format('dddd[,] MMMM Do')
-// console.log(moment().format());
-// console.log(moment());
-// console.log()
+
